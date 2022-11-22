83a8696175f79c6ab9818a3d9afc884daa8d16b5
peter-030 <chuams-wm19@student.tarc.edu.my>
2022-11-22 14:37:24 +0800

    peter 1.1

---
+++ app/src/main/java/com/example/fypcanteensystem/FoodOrderingModule.kt
@@ -0,0 +1,11 @@
+package com.example.fypcanteensystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodOrderingModule : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_ordering_module)
+    }
+}
